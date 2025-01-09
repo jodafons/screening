be82e9995a665bde31a78cf7bd49925c91a5eb7c
jodafons <jodafons@lps.ufrj.br>
2025-01-09 10:53:51 -0300

    refac

--- activate.sh
+++ activate.sh
@@ -2,6 +2,32 @@
 
 mode=$1
 
+# docker
+export DOCKER_NAMESPACE=screening
+
+# virtualenv name
+export VIRTUALENV_NAMESPACE=screening-env
+
+# lab storage environs
+export PROJECT_DIR=/mnt/brics_data
+export DOCKER_STORAGE=$PROJECT_DIR
+
+# data input
+export DATA_DIR=$PROJECT_DIR/datasets
+
+# repo
+export REPO_DIR=$PWD
+
+export DATABASE_SERVER_URL=$POSTGRES_SERVER_URL
+
+# logger level
+export LOGURO_LEVEL="INFO"
+
+# dorothy token
+export DOROTHY_TOKEN="b16fe0fc92088c4840a98160f3848839e68b1148"
+
+
+
 if [ -d "$VIRTUALENV_NAMESPACE" ]; then
     echo "$VIRTUALENV_NAMESPACE exists."
     source $VIRTUALENV_NAMESPACE/bin/activate
---
+++ servers/database/db/models.py
@@ -0,0 +1,74 @@
+
+__all__ = ["Dataset", "Image", "ImageInfo"]
+
+
+import datetime, traceback, os
+
+from sqlalchemy import create_engine, Column, Boolean, Float, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship, declarative_base
+
+
+Base = declarative_base()
+
+
+class Fold(Base):
+    __tablename__ = "fold"
+
+
+
+class Dataset(Base):
+    __tablename__ = "dataset"
+
+    id          = Column(Integer, primary_key = True)
+    name        = Column(String, unique=True)
+    dataset_id  = Column(String, unique=True)
+    public      = Column(Boolean, default=False)
+    synthetic   = Column(Boolean, default=False)
+    last_update = Column(DateTime)
+    comment     = Column(String)
+
+    # Foreign  
+    images      = relationship("Image", order_by="Image.id", back_populates="dataset")
+
+
+
+class Image(Base):
+    __tablename__ = "image"
+
+    id               = Column(Integer, primary_key = True)
+    image_id         = Column(String, unique=True)
+    image_hash       = Column(String)
+    date_acquisition = Column(Datetime)
+    date_insertion   = Column(DateTime)
+    project_id       = Column(String)
+
+    # Foreign
+    dataset    = relationship("Dataset", back_populates="images")
+    datasetid  = Column(Integer, ForeignKey('dataset.id'))
+  
+
+
+class ImageInfo(Base):
+
+    __tablename___ = "image_info"
+    id             = Column(Integer, primary_key = True)
+    version        = Column(Integer, default=0)
+    imageinfo_id   = Column(String, unique=True)
+    image_id       = Column(String)
+    has_tb         = Column(Boolean)
+    gender         = Column(Char)
+    age            = Column(Integer)
+    date_exam      = Column(DateTime)
+    date_insertion = Column(DateTime)
+
+    under_penetrated                = Column(Boolean)
+    over_penetrated                 = Column(Boolean)
+    costophrenic_cropped            = Column(Boolean)
+    apices_cropped                  = Column(Boolean)
+    reliable_radiography            = Column(Boolean)
+    minimum_interpretation_quality  = Column(Boolean)
+    performed_by                    = Column(String)
+    comment                         = Column(String)
+    metadata                        = Column(String, default="{}")
+
+
